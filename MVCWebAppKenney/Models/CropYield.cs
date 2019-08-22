9db0649331e0703b42c643abba09261c33595a8b
Tyler Kenney <contact@tgkenney.dev>
2019-08-22 12:28:35 -0400

    Added CropYield and Class diagram

---
+++ MVCWebAppKenney/Models/CropYield.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCWebAppKenney.Models
+{
+    public class CropYield
+    {
+        [Key]
+        public int CropYieldID { get; set; }
+        public double YieldAmount { get; set; }
+        public DateTime YieldYear { get; set; }
+
+        // Link to Crop
+        public int CropID { get; set; }
+        [ForeignKey("CropID")]
+        public Crop Crop { get; set; }
+
+        // Link to Farm
+        public int FarmID { get; set; }
+        [ForeignKey("FarmID")]
+        public Farm Farm { get; set; }
+    }
+}
