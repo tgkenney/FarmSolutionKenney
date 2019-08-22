2a558f7717cc2ec3a3e3f547439fbffcbdb535bc
Tyler Kenney <contact@tgkenney.dev>
2019-08-22 13:37:14 -0400

    Started work on making CropYields searchable

--- MVCWebAppKenney/Controllers/CropsController.cs
+++ MVCWebAppKenney/Controllers/CropsController.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MVCWebAppKenney.Data;
 using MVCWebAppKenney.Models;
+using MVCWebAppKenney.ViewModels;
 
 namespace MVCWebAppKenney.Controllers
 {
@@ -35,6 +38,29 @@ namespace MVCWebAppKenney.Controllers
             List<Crop> cropList = database.Crops.Include(c => c.Classification).ToList<Crop>();
 
             return View(cropList);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult SearchCropYields()
+        {
+            ViewData["CropList"] = new SelectList(database.Crops, "CropID", "CropName");
+            ViewData["FarmList"] = new SelectList(database.Farms, "FarmID", "FarmName");
+
+            SearchCropYieldsViewModel model = new SearchCropYieldsViewModel();
+
+            return View(model);
+        }
+        [HttpPost]
+        [Authorize]
+        public IActionResult SearchCropYields(SearchCropYieldsViewModel model)
+        {
+            ViewData["CropList"] = new SelectList(database.Crops, "CropID", "CropName");
+            ViewData["FarmList"] = new SelectList(database.Farms, "FarmID", "FarmName");
+
+            // IQueryable<CropYield> cropYieldsList = database.CropYields.Include(cY => cY.Crop).
+
+            return View(model);
         }
     }
 }
---
+++ MVCWebAppKenney/ViewModels/SearchCropYieldsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MVCWebAppKenney.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCWebAppKenney.ViewModels
+{
+    public class SearchCropYieldsViewModel
+    {
+        // User inputs for search
+        public int? CropID { get; set; }
+        public int? FarmID { get; set; }
+        public int SearchProductionYear { get; set; }
+
+        // Search result
+        public List<CropYield> CropYieldList { get; set; }
+
+    }
+}
